846c4d03512f16e56d205a7f3056789ef32650b9
Prajwalprakash3722 <prajwalprakash3722@gmail.com>
2021-05-14 19:02:56 +0530

    changes

--- home.py
+++ home.py
@@ -11,11 +11,13 @@ from main import *
 from request import *
 from createtable import create_table
 import streamlit.components.v1 as components
-
+import webbrowser as web
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 
 app_header()
 main()
 st.markdown(
-    "Any Issues, Just tweet it and mention me, I'll try my best to address them")
+    "Any Issues, Just tweet it and mention me, or go to the following link in github, I'll try my best to address them")
 components.html("""<a href="https://twitter.com/intent/tweet?screen_name=prakash_prajwal&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-lang="en" data-show-count="false">Tweet to @prakash_prajwal</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>""")
+if st.button("Report any issues in Github"):
+    web.open_new_tab("https://github.com/Prajwalprakash3722/Vaccine_avaliabality/issues")
